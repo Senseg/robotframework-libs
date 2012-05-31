0c8e158c4fe108eabb6732c9768434797a40a754
Pekka Nikander <pekka.nikander@senseg.com>
2012-05-31 14:42:43 +0300

    Add first versions of units.py and adb.py

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ robotframework-libs
 ===================
 
 RobotFramework libs, mostly related to Android testing
+
+* units.py -- parse numbers with units, e.g. 100kHz or 10ms
+* adb.py -- talk to an Android device
---
+++ units.py
@@ -0,0 +1,14 @@
+"""Helper routine to parse numbers with units."""
+
+def parse_unit(s, expected_unit=None, unit_required=False):
+    muls = { None: 1.0, 'k': 1000.0, 'm': 1.0/1000, 'u': 1.0/1000000, 'n': 1.0/1000000000 }
+    m = re.match(r'([0-9\.eE+-]+) *(k|m|u|n)?(s|discharger_clocks|cycles|Hz|%|V)?', s)
+    if not m:
+        raise RuntimeError("not a number + unit: " + s)
+    (value, prefix, unit) = m.groups()
+    if expected_unit:
+        if unit or unit_required:
+            if expected_unit != unit:
+                raise RuntimeError("Expecting unit " + expected_unit + ", found unit " + unit)
+    return (float(value) * muls[prefix], unit)
+
